b154d0e83051f18f3f5b1cf328e3ff4626554580
Petemc121 <petemc121@gmail.com>
2022-01-12 10:44:03 +0000

    worked on database connection issue

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const router = require("express").Router();
 const app = express();
 const cors = require("cors");
 const pool = require("./database");
@@ -7,14 +8,17 @@ const path = require("path");
 
 app.use(cors());
 app.use(express.json()); //req.body
+app.use("/", router);
 app.use(express.static(path.join(__dirname, "build")));
 
 app.post("/members", async (req, res) => {
   try {
     const { user_name, user_email, user_password } = req.body;
+    console.log(req.body);
+    console.log(user_name);
 
     const newMember = await pool.query(
-      `INSERT INTO members (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *`,
+      "INSERT INTO members (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
       [user_name, user_email, user_password]
     );
 
